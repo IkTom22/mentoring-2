a22aa13041d5c95fdb85835d7cf5b0716b40ed30
Iko <ikotominaga@gmail.com>
2024-07-05 18:14:02 +1200

    azure and terraform setted up

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ARM_CLIENT_SECRET" {
+    type = string
+    description = "client_secret"
+}
+variable "ARM_CLIENT_ID" {
+    type = string
+    description = "client_secret"
+}
+variable "ARM_SUBSCRIPTION_ID" {
+    type = string
+    description = "client_secret"
+}
+variable "ARM_TENANT_ID" {
+    type = string
+    description = "client_secret"
+}
+
